f54c0f4b490d4951be7fdd25a8a556ae38024901
Scott McCullagh <scott.mccullagh@gmail.com>
2018-01-11 21:58:42 +0000

    Added eslint-airbnb to the project

--- app.js
+++ app.js
@@ -3,25 +3,27 @@ const participants = ['A', 'B', 'C'];
 /**
  * Simulates each player picking a name one at a time and rerolling if they get themselves
  */
-const pickFromBagOfNames = players => {
-    let num;
+const pickFromBagOfNames = (players) => {
+  let num;
 
-    const output = {};
-    const names = [...players];
+  const output = {};
+  const names = [...players];
 
-    players.forEach(player => {
-        let name = player;
-        while(player == name){
-            num = getRandomNumber(names.length);
-            name = names[num];
-        }
-        output[player] = names[num];
-        names.splice(num,1);
-    });
+  const getRandomNumber = (max) => {
+    Math.floor(Math.random() * max);
+  };
 
-    return output;
-}
+  players.forEach((player) => {
+    let name = player;
+    while (player === name) {
+      num = getRandomNumber(names.length);
+      name = names[num];
+    }
+    output[player] = names[num];
+    names.splice(num, 1);
+  });
 
-const getRandomNumber = max => {
-    return Math.floor(Math.random()*max);
-}
+  return output;
+};
+
+pickFromBagOfNames(participants);
