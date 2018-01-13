4b26d125a79097c3f44881d9a6a4917de269d1f3
Scott McCullagh <scott.mccullagh@gmail.com>
2018-01-13 10:40:19 +0000

    Tidied up email.js and exported the sendEmail function

--- email.js
+++ email.js
@@ -4,21 +4,31 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'mccullagh.secret.santa@gmail.com',
-    pass: '',
+    pass: 'secret-santa',
   },
 });
 
-const mailOptions = {
-  from: 'mccullagh.secret.santa@gmail.com',
-  to: '',
-  subject: 'This is a test message',
-  html: '<p>This is a test messsage, sent from nodejs',
+// TODO: Check that values passed in are valid
+const createMailOptions = (recipient, subject, html) =>
+  ({
+    from: 'mccullagh.secret.santa@gmail.com',
+    to: recipient,
+    subject,
+    html,
+  });
+
+// TODO: add logger and remove console.log statements
+const sendEmail = (recipient, subject, htmlMessage) => {
+  const mailOptions = createMailOptions(recipient, subject, htmlMessage);
+  transporter.sendMail(mailOptions)
+    .then((info) => {
+      console.log('Successfully sent email to', recipient, 'with subject', subject);
+      console.log(info);
+    })
+    .catch((err) => {
+      console.log('Error sending email to', recipient, 'with subject', subject);
+      console.log(err);
+    });
 };
 
-transporter.sendMail(mailOptions, (err, info) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(info);
-  }
-});
+module.exports.sendEmail = sendEmail;
