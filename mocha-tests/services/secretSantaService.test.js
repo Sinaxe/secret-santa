50b0c65d6ac9cf70610d87bfd45d3b5a7637de7e
Scott McCullagh <scott.mccullagh@gmail.com>
2018-03-31 12:00:06 +0100

    Add test for controller. ESLint doesnt like mocha before/after functions or rewire's __set__

--- mocha-tests/services/secretSantaService.test.js
+++ mocha-tests/services/secretSantaService.test.js
@@ -1,15 +1,23 @@
 const { expect } = require('chai');
-const secretSanta = require('../../api/services/secretSantaService');
+const secretSantaService = require('../../api/services/secretSantaService');
 
 describe('secret santa', () => {
   describe('pickNames', () => {
     it('should randomly assign given players so that no one has themselves', () => {
       const players = ['cat', 'badger', 'hedgehog'];
-      const result = secretSanta.pickNames(players);
+      const result = secretSantaService.pickNames(players);
 
       expect(result.cat).to.not.equal('cat');
       expect(result.badger).to.not.equal('badger');
       expect(result.hedgehog).to.not.equal('hedgehog');
+    });
+
+    it('should return an error if a single name is passed in', () => {
+      const players = ['cat'];
+      const result = secretSantaService.pickNames(players);
+      const expectedError = { error: 'Unable to distribute - only one name passed in' };
+
+      expect(result).to.eql(expectedError);
     });
   });
 });
