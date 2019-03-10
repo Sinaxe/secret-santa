6fee7f173ba76952ceed595e95d9d39c30eed5d0
Scott McCullagh <scott.mccullagh@gmail.com>
2019-03-10 18:15:38 +0000

    Giving typescript a whirl

--- api/routes/secretSantaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const secretSantaController = require('../controllers/secretSantaController');
-
-const router = express.Router();
-
-router.post('/', secretSantaController.handleSecretSantaRequest);
-
-module.exports = router;
